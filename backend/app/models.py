abce2087db523f62ba21bf0c41e30866446ca5f8
Muhammad Taha Mirza <tahaseopro@gmail.com>
2026-07-12 00:49:06 +0500

    alembic added for sqlite

--- backend/app/models.py
+++ backend/app/models.py
@@ -7,3 +7,25 @@ class Lead(SQLModel, table=True):
     email : str
     company_name: str | None = None
 
+
+
+class MailAccount(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    email: str
+    password : str
+    smtp_server : str
+    port : int
+
+
+class template(SQLModel, table=True):
+	id: int | None = Field(default=None, primary_key=True)
+	subject: str
+	body: str
+
+
+class Campaign(SQLModel, table=True):
+	id: int | None = Field(default=None, primary_key=True)
+
+
+
+
--- backend/app/routes/leadRoute.py
+++ backend/app/routes/leadRoute.py
@@ -1,7 +1,7 @@
 from sqlmodel import Session, select
 from fastapi import APIRouter, Depends
 from schemas import UserCreate
-from models import Lead
+from models import Lead, MailAccount
 from db import engine, get_session
 router = APIRouter()
 
@@ -18,13 +18,42 @@ def create_lead(user: Lead):
 		"status" : "success"
 	}
 
+
+
+@router.post("/add-email")
+def add_email(email: MailAccount):
+	with Session(engine)as session:
+		session.add(email)
+		response =session.commit()
+		print(response)
+		session.close()
+
+
+	return {
+		"status" : "email added"
+	}
+
+@router.get("/all-emails")
+def get_all_email(session : Session =  Depends(get_session)):
+	statemet =select(MailAccount)
+	output = session.exec(statemet)
+
+	# print(type(output._allrows()))
+	data = output.all()
+
+	# print(output)
+	print(data)
+
+	return {"emails" : data}
+
 @router.get("/lead")
 def get_all_leads(session : Session =  Depends(get_session)):
 
 	statement = select(Lead)
 
 	results = session.exec(statement)
+	data = results.all()
 
 	return{
-		"leads" : results.all()
+		"leads" : data
 	}
